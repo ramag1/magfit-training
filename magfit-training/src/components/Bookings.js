0d77b0521610afe3c2ae393bbe2535bdee492baa
rmagley <rachelmagley@gmail.com>
2022-03-21 16:35:14 -0700

    Add Booking info

--- magfit-training/src/components/Bookings.js
+++ magfit-training/src/components/Bookings.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function Bookings(props) {
     return (
-        <div>
-            bookings 
-        </div>
-    );
+			<div>
+				All bookings are managed through Mindbody Online. Please visit the link below to review pricing, packages and secure a spot for class. <br/>
+				<a
+					 href= 'https://www.mindbodyonline.com/explore/locations/magfit-training' target='_blank'>
+					MAGFIT
+				</a>
+			</div>
+		);
 }
 
 export default Bookings;
